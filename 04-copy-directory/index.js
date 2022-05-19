746b3b37abe810b71b9e6b0f7916b35a1cc86bd1
Maria22 <m.ushakova22@gmail.com>
2022-05-19 13:53:09 +0200

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const { readdir, mkdir, copyFile, unlink } = require('fs/promises');
+const path = require('path');
 
+const pathToDest = path.join(__dirname, './files-copy');
+const pathToSource = path.join(__dirname, './files');
+
+mkdir(pathToDest, { recursive: true }, (error) => {
+  try {
+    if (error) throw error;
+  }
+  catch (error) {
+    console.log(error);
+  }
+});
+
+(async function copyFiles() {
+  const filesDest = await readdir(pathToDest, { encoding: 'utf-8', withFileTypes: true });
+  filesDest.forEach((file) =>
+    unlink(`${pathToDest}/${file.name}`, error => {
+      try {
+        if (error) throw error;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }));
+  const files = await readdir(pathToSource, { encoding: 'utf-8', withFileTypes: true });
+  files.forEach((file) => copyFile(`${pathToSource}/${file.name}`, `${pathToDest}/${file.name}`, 0, (error) => {
+    try {
+      if (error) throw error;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }));
+})();
+console.log('Files were copied!');
